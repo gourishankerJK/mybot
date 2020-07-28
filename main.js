8c65f3465736249acdd752848c786f3cba474733
Gouri Shanker <shanker.gouri@hotmail.com>
2020-07-28 18:28:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -29,23 +29,65 @@ function addBotText(text) {
 
 function botVoice(message) {
     const speech = new SpeechSynthesisUtterance();
-    speech.text = "Sorry, I did not understand that.";
+    speech.text = "apologies!!!. i am still in developing mode. I don't have any idea about it.";
 
     if (message.includes('how are you')) {
       speech.text = "I am fine, thanks. How are you?";
     }
-
-    if (message.includes('fine')) {
-      speech.text = "Nice to hear that. How can I assist you today?";
+    else if(message.includes('are you listening')){
+      speech.text = " yes I am listening.!!";
+      window.speechSynthesis.speak(speech);
+      speech.text="press Activate Voice button to make me listen your voice .........thanks";
+    }
+    else if (message.includes('who are you')){
+      speech.text = "hey i am aryan's assistant ... gouri invented me and currently aryan is developing me . you can call me jarvis's friend ..can i do something for you.."
+    }
+    else if (message.includes('thanks')||message.includes('thank you')){
+      speech.text="most welcome sir it's my duty ... can i do something for you ..";
+    }
+    else if (message.includes('you are cute')||message.includes("you are so cute")||message.includes("you are intelligent")){
+      speech.text="thanks sir .. .......you are more  intelligent and cute than me.";
     }
 
-    if (message.includes('weather')) {
-      speech.text = "Of course. Where are you currently?";
+    else if(message.includes('open Google')){
+      window.open('https://www.google.com','_blank');
+      return 0;
+
+    }
+    else if(message.includes('open YouTube')){
+      window.open('https://www.youtube.com','_blank');
+      return 0;
+
+    }
+    else if(message.includes('shut up')||message.includes('get out')){
+      speech.text=" i am sorry sir ... bye bye";
+      window.speechSynthesis.speak(speech);
+      window.open("index.html",'_self');
+      return 0;
+    }
+    else if(message.includes('search about')||message.includes('search')){
+      text = message.split(" ");
+      if (text[0]=='search'&&text[1]=='about'){
+        text.shift();
+        text.shift();
+      }
+      else{
+        text.shift();
+      }
+      search = text.join(" ");
+      url = 'https://google.com/search?q='+search;
+      window.open(url);
+      return 0;
+    }
+    else if(message.includes('Wikipedia')){
+      text = message.split(" ");
+      text.pop();
+      search = text.join(" ");
+      url = "https://en.wikipedia.org/wiki/"+search;
+      window.open(url);
+      return 0;
     }
 
-    if (message.includes('Kashmir')) {
-      speech.text = "It is 18 degrees and sunny.";
-    }
 
     speech.volume = 1;
     speech.rate = 1;
@@ -57,6 +99,7 @@ function botVoice(message) {
 
 recorder.onstart = () => {
   console.log('Voice activated');
+
 };
 
 recorder.onresult = (event) => {
@@ -68,5 +111,11 @@ recorder.onresult = (event) => {
 };
 
 voice.addEventListener('click', () =>{
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = " i am listening";
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+  window.speechSynthesis.speak(speech);
   recorder.start();
 });
